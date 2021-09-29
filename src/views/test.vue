9aabf3b0d1063944763ea806aac2cbeebdc237c8
杜雨新 <9806588+du-yuxin123@user.noreply.gitee.com>
2021-09-29 09:51:31 +0000

    !3 MD5加密 * 添加md5转化

--- src/views/test.vue
+++ src/views/test.vue
@@ -2,16 +2,16 @@
   <v-row>
     <v-col>
       <v-card>
-        <v-card-title> 测试1 </v-card-title>
+        <v-card-title> 测试1</v-card-title>
         <v-card-subtitle>
-          <v-btn @click="myGetUserList"> 获取所有用户信息 </v-btn>
+          <v-btn @click="myGetUserList"> 获取所有用户信息</v-btn>
         </v-card-subtitle>
         <v-card-subtitle>
           <v-list>
             <v-list-item v-for="item in user_list" :key="item.id">
               <v-list-item-content>
-                ID: {{ item.id }}<br />
-                NAME: {{ item.username }}<br />
+                ID: {{ item.id }}<br/>
+                NAME: {{ item.username }}<br/>
                 NICKNAME: {{ item.nickname }}
               </v-list-item-content>
             </v-list-item>
@@ -21,15 +21,33 @@
     </v-col>
     <v-col>
       <v-card>
-        <v-card-title> 测试2 </v-card-title>
+        <v-card-title> 测试2</v-card-title>
         <v-card-subtitle>
-          <v-btn @click="myGetUser"> 获取当前用户信息 </v-btn>
+          <v-btn @click="myGetUser"> 获取当前用户信息</v-btn>
         </v-card-subtitle>
         <v-card-title>当前用户</v-card-title>
         <v-card-subtitle>
-          ID: {{ visitingUserInfo.id }}<br />
-          NAME: {{ visitingUserInfo.username }}<br />
+          ID: {{ visitingUserInfo.id }}<br/>
+          NAME: {{ visitingUserInfo.username }}<br/>
           NICKNAME: {{ visitingUserInfo.nickname }}
+        </v-card-subtitle>
+      </v-card>
+    </v-col>
+    <v-col>
+      <v-card>
+        <v-card-title> 测试3 md5</v-card-title>
+        <v-card-subtitle>
+          <v-text-field
+              label="明文"
+              hide-details="auto"
+              v-model="originCode"
+          ></v-text-field>
+          <br>
+          <v-btn @click="getPassword"> submit </v-btn>
+        </v-card-subtitle>
+        <v-card-title> 密文 </v-card-title>
+        <v-card-subtitle>
+          {{password}}
         </v-card-subtitle>
       </v-card>
     </v-col>
@@ -37,7 +55,8 @@
 </template>
 
 <script>
-import { getUserList, getUserInfo } from "@/api/user.js";
+import {getUserList, getUserInfo} from "@/api/user.js";
+import {hex_md5} from '@/api/md5.js'
 
 export default {
   name: "test",
@@ -46,6 +65,8 @@ export default {
     return {
       user_list: {},
       visitingUserInfo: "",
+      originCode: null,
+      password: null,
     };
   },
 
@@ -66,6 +87,12 @@ export default {
       let res = await getUserInfo();
       console.log(res);
       this.visitingUserInfo = res.data;
+    },
+
+    getPassword() {
+      const originString = String(this.originCode)
+      this.password = hex_md5(originString)
+      console.log(this.password)
     },
   },
 };
